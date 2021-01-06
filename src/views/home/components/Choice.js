42d41cd52d15be9d96affb7c15ca1a7f65389915
unknown <vimalkuriensam@gmail.com>
2021-01-06 18:56:45 +0530

    added option states

--- src/views/home/components/Choice.js
+++ src/views/home/components/Choice.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import Options from "./options/Options";
 
 const Choice = () => (
-    <div className="home__choice">Choice</div>
-)
+  <div className="home__choice">
+    <div className="home__options">
+      <Options />
+    </div>
+  </div>
+);
 
 export default Choice;
