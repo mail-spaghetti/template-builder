3679eb19e8da7d92389b5bf7bdeb2ce1592a8412
unknown <vimalkuriensam@gmail.com>
2021-01-06 19:46:41 +0530

    added option change feature

--- src/views/home/components/Choice.js
+++ src/views/home/components/Choice.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+
+import { changeSelection } from "../../../actions/options.action";
 import Options from "./options/Options";
 
 class Choice extends Component {
+  onHandleSelection = (selection) =>
+    this.props.dispatch(changeSelection({ selection }));
+
   render() {
     return (
       <div className="home__choice">
         <div className="home__options">
-          <Options selection={this.props.selection} />
+          <Options
+            selection={this.props.selection}
+            onHandleSelection={this.onHandleSelection}
+          />
         </div>
       </div>
     );
