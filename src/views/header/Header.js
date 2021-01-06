3eb7cbe93406b4ee818b412be3aa4b118f0a64cb
unknown <vimalkuriensam@gmail.com>
2021-01-06 09:55:11 +0530

    added header userdetails

--- src/views/header/Header.js
+++ src/views/header/Header.js
@@ -4,7 +4,7 @@ import { ButtonData } from "./data";
 
 import Logo from "../../utils/icons/Logo";
 
-const Header = () => (
+const Header = (props) => (
   <div className="header">
     <div className="header__logo">
       <Logo />
@@ -13,6 +13,16 @@ const Header = () => (
       {ButtonData.map((d, idx) => (
         <Button key={idx} variant="secondary" text={d.text} />
       ))}
+    </div>
+    <div className="header__auth">
+      <span className="header__caption">User Name</span>
+      <span>
+        <figure className="header__shape">
+          {props.img ? (
+            <img className="header__img" alt="Authenticated user" />
+          ) : null}
+        </figure>
+      </span>
     </div>
   </div>
 );
