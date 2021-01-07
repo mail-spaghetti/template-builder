30363e08ecb14c0f0b199056c7ae9559308e5c30
unknown <vimalkuriensam@gmail.com>
2021-01-07 08:49:13 +0530

    added component icons

---
+++ src/utils/icons/Text.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Text = () => (
+  <svg
+    width="29"
+    height="26"
+    viewBox="0 0 29 26"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clipPath="url(#clip0)">
+      <path
+        d="M10.818 1.803H18.03V5.409H19.83V0H0V5.409H1.8V1.803H9.015V23.439H5.409V25.239H14.424V23.439H10.818V1.803Z"
+        fill="#141414"
+      />
+      <path
+        d="M12.621 7.21199V10.818H14.421V9.01799H19.83V23.442H16.224V25.242H25.239V23.442H21.633V9.01498H27.042V10.815H28.842V7.20898L12.621 7.21199Z"
+        fill="#141414"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0">
+        <rect width="28.848" height="25.242" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default Text;
--- src/views/home/data/index.js
+++ src/views/home/data/index.js
@@ -1,1 +1,40 @@
 export const OPTIONS = ["Components", "Structure", "Page Settings"];
+
+export const CARDS = [
+  {
+    text: "TEXT",
+    icon: "Text",
+  },
+  {
+    text: "IMAGE",
+    icon: "Image",
+  },
+  {
+    text: "GIF",
+    icon: "Gif",
+  },
+  {
+    text: "BUTTON",
+    icon: "Button",
+  },
+  {
+    text: "DIVIDER",
+    icon: "Divider",
+  },
+  {
+    text: "SPACER",
+    icon: "Spacer",
+  },
+  {
+    text: "SOCIAL",
+    icon: "Social",
+  },
+  {
+    text: "VIDEO",
+    icon: "Video",
+  },
+  {
+    text: "HTML",
+    icon: "Code",
+  },
+];
