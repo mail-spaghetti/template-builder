8ac15ac569a0d2fcce671867f5f45eae218c5edf
unknown <vimalkuriensam@gmail.com>
2021-01-07 08:49:13 +0530

    added component icons

---
+++ src/utils/icons/Spacer.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Spacer = () => (
+  <svg
+    width="35"
+    height="26"
+    viewBox="0 0 35 26"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clipPath="url(#clip0)">
+      <path
+        d="M17.7249 0L14.325 3.36L15.252 4.297L17.062 2.507V7.422H18.3799V2.522L20.1539 4.29601L21.086 3.364L17.7249 0Z"
+        fill="#141414"
+      />
+      <path
+        d="M17.7249 25.993L14.325 22.633L15.252 21.696L17.062 23.486V18.57H18.3799V23.47L20.1539 21.696L21.086 22.628L17.7249 25.993Z"
+        fill="#141414"
+      />
+      <path
+        d="M32 10.097H3C1.61929 10.097 0.5 11.2163 0.5 12.597V13.597C0.5 14.9777 1.61929 16.097 3 16.097H32C33.3807 16.097 34.5 14.9777 34.5 13.597V12.597C34.5 11.2163 33.3807 10.097 32 10.097Z"
+        stroke="#141414"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0">
+        <rect width="35" height="25.993" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default Spacer;
