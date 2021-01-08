8944b2515b844d549639030ce322be53f20f2f34
unknown <vimalkuriensam@gmail.com>
2021-01-08 05:52:16 +0530

    added settings reducer

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -3,6 +3,7 @@ import thunk from "redux-thunk";
 import logger from "redux-logger";
 
 import optionsReducer from "../reducers/options.reducer";
+import settingsReducer from "../reducers/settings.reducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -10,6 +11,7 @@ export default () => {
   const store = createStore(
     combineReducers({
       option: optionsReducer,
+      settings: settingsReducer,
     }),
     composeEnhancers(applyMiddleware(thunk, logger))
   );
