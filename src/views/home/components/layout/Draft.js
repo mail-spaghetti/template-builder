a68f8facff647e3557fdb6b3d5c0095afbb4ee72
unknown <vimalkuriensam@gmail.com>
2021-01-08 10:28:17 +0530

    added init layout elements

--- src/views/home/components/layout/Draft.js
+++ src/views/home/components/layout/Draft.js
@@ -1,5 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { connect } from "react-redux";
+
+import Add from "../../../../utils/icons/Add";
+import Copy from "../../../../utils/icons/Copy";
+import Delete from "../../../../utils/icons/Delete";
+import Drop from "../../../../utils/icons/Drop";
+import Move from "../../../../utils/icons/Move";
 
 const Layout = ({ height, component }) => {
   useEffect(() => {
@@ -11,17 +17,74 @@ const Layout = ({ height, component }) => {
   const setInitLayout = () => {
     let layout = [];
     for (let i = 0; i < parseInt(parseInt(height.split("px")) / 15); i++) {
+      let content = null;
+      if (component.activeContent === i + 1) {
+        content = (
+          <div
+            key={i}
+            className="draft__blocks draft__blocks--active"
+            style={{
+              height: "14rem",
+              padding: "2.5rem 14rem",
+              position: "relative",
+            }}
+          >
+            {addActiveSnap()}
+          </div>
+        );
+      } else {
+        content = (
+          <div
+            key={i}
+            className="draft__blocks"
+            style={{ height: "14rem" }}
+          ></div>
+        );
+      }
+
       layout = [
         ...layout,
         {
           [i]: {
-            content: <div key={i} style={{height: "5rem"}}></div>,
+            content,
           },
         },
       ];
     }
     setContents(layout);
   };
+
+  const addActiveSnap = () => (
+    <Fragment>
+      <div className="draft__snap draft__snap--1">
+        <span>
+          <Add />
+        </span>
+        <span>
+          <Delete />
+        </span>
+      </div>
+      <div className="draft__snap draft__snap--2">
+        <span>
+          <Add />
+        </span>
+        <span>
+          <Move />
+        </span>
+      </div>
+      <div className="draft__snap draft__snap--3">
+        <span>
+          <Copy />
+        </span>
+      </div>
+      <div className="draft__contents">
+        <div>
+          <Drop />
+        </div>
+        <div>Drop content here</div>
+      </div>
+    </Fragment>
+  );
   return (
     <section className="section-draft" style={{ height }}>
       <div>
