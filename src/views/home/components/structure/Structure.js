8e4e79dab778bdca2397bcf487c322c92d8be31f
unknown <vimalkuriensam@gmail.com>
2021-01-10 18:11:50 +0530

    added structure reducer

--- src/views/home/components/structure/Structure.js
+++ src/views/home/components/structure/Structure.js
@@ -11,13 +11,21 @@ const Structure = () => {
     const structs = structRows.map((row, index) => (
       <div className="a-row" key={index}>
         {row.map((struct, idx) => (
-          <div className="a-col-1-of-2" key={`${index}-${idx}`}>
+          <div
+            className="a-col-1-of-2"
+            onClick={onHandleStructSelection}
+            key={`${index}-${idx}`}
+          >
             <Structs struct={struct} />
           </div>
         ))}
       </div>
     ));
     return structs;
+  };
+
+  const onHandleStructSelection = () => {
+    console.log("clicked");
   };
   return (
     <div className="card__structWrapper">
