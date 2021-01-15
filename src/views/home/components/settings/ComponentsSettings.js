d682539ab853cb76c362514e7bc538223cb312be
unknown <vimalkuriensam@gmail.com>
2021-01-15 08:42:21 +0530

    added text settings ui

--- src/views/home/components/settings/ComponentsSettings.js
+++ src/views/home/components/settings/ComponentsSettings.js
@@ -1,10 +1,27 @@
 import React from "react";
 
+import Text from "../../../../components/atoms/Text";
+import LeftArrow from "../../../../utils/icons/LeftArrow";
+
+import { setSelected } from "../../../../actions/options.action";
+
 const ComponentsSettings = ({ type, ...rest }) => {
+  const onHandleSettingsExit = () => rest.dispatch(setSelected({ selected: false }));
   return (
-    <div>
+    <div className="settings">
+      <div className="settings__type u-margin-top-light">
+        <Text className="text-tertiary" content={type} />
+        <div className="settings__back" onClick={onHandleSettingsExit}>
+          <span>
+            <LeftArrow />
+          </span>
+          <span>
+            <Text content="BACK" />
+          </span>
+        </div>
+      </div>
       {require(`./${type.replace(" Settings", "")}Settings.js`).default({
-        type: type || selection,
+        type: type,
         ...rest,
       })}
     </div>
--- src/views/home/components/settings/TextSettings.js
+++ src/views/home/components/settings/TextSettings.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { Fragment } from "react";
+
+import HorizontalRule from "../../../../components/atoms/HorizontalRule";
+import Text from "../../../../components/atoms/Text";
+import Slider from "../../../../components/molecules/Slider";
+import Knob from "../../../../components/organisms/Knob";
 
 const TextSettings = () => (
-    <div>textSettings</div>
-)
+  <div className="u-padding-light">
+    <Text className="settings__heading" content="Margin" />
+    <div>
+      <div className="row">
+        <div className="col-1-of-2">
+          <Text content="Margin Top" />
+          <Knob />
+        </div>
+        <div className="col-1-of-2">
+          <Text content="Margin Bottom" />
+          <Knob />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-1-of-2">
+          <Text content="Margin Left" />
+          <Knob />
+        </div>
+        <div className="col-1-of-2">
+          <Text content="Margin Right" />
+          <Knob />
+        </div>
+      </div>
+    </div>
+    <HorizontalRule />
+    <div className="u-display-flex u-display-flex--2">
+      <div>
+        <Slider />
+      </div>
+      <div>
+        <Text content="Hide on mobile" />
+      </div>
+    </div>
+    <div className="u-display-flex u-display-flex--2 u-margin-top-small">
+      <div>
+        <Slider />
+      </div>
+      <div>
+        <Text content="Hide on desktop" />
+      </div>
+    </div>
+  </div>
+);
 
 export default TextSettings;
